2aa5e06ae33f12487d94301ff1ceb8e48d3268fb
Milton Ezequiel Ojeda <mojeda@secco.com.ar>
2022-05-24 09:16:32 -0300

    exercise variables and operators

---
+++ exercises/01_variables_and_operators.js
@@ -0,0 +1,16 @@
+/* Crear dos variables de tipo String y concatenarlas 
+ guardando el resultado en una 3er variable. */
+
+var string1 = 'me estoy';
+var string2 = 'concatenando';
+var resultado = string1+' '+string2;
+console.log(resultado);
+
+/* Crear dos variables de tipo String y sumar el largo de cada variable
+ (cantidad de letras del string) guardando el resultado de la suma en 
+ una 3er variable (utilizar length). */ 
+
+ var str1 = 'hola';
+ var str2 = 'mundo';
+ var strlength = str1.length + str2.length ;
+ console.log('longitud total: '+strlength)
